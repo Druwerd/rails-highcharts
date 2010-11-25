66c45317a47a2de76a36dd55b9b195a4f9f59fc3
dru.ibarra <druibarra@ubuntu.(none)>
2010-11-25 00:46:00 -0800

    modified chart display, improved rrd fetch function

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -3,8 +3,9 @@ require 'rrd'
 class Chart < ActiveRecord::Base
 	def self.get_rrd_data(rrd_file)
 		rrd = RRD::Base.new(rrd_file)
-		d = rrd.fetch("AVERAGE", {:start => Time.now - 30})
+		d = rrd.fetch("AVERAGE", {:start => Time.now - 20})
 		d = d.select{|i| i.last.kind_of?(Float) && !i.last.nan?}
-		d.last[1]
+		load = d.last[1] if d.last 
+		load || 0.0
 	end
 end
